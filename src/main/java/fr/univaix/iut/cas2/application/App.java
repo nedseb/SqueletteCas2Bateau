8549d45cb6302588395b3f7ea380716338c2c3c0
Sébastien NEDJAR <nedseb@goldaire.com>
2012-03-29 11:34:45 +0200

    Ajout de l'option pour générer les scripts SQL

--- src/main/java/fr/univaix/iut/cas2/application/App.java
+++ src/main/java/fr/univaix/iut/cas2/application/App.java
@@ -4,10 +4,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- * Hello world!
- *
- */
+import fr.univaix.iut.cas2.beans.Modele;
+import fr.univaix.iut.cas2.dao.DAOBateau;
+import fr.univaix.iut.cas2.dao.DAOFactory;
+import fr.univaix.iut.cas2.dao.jpa.DAOFactoryJPA;
+
 public class App 
 {
     public static void main( String[] args )
@@ -15,7 +16,12 @@ public class App
         // Initializes the Entity manager
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionPortuaire");
         EntityManager em = emf.createEntityManager();
-
+        
+        DAOFactory daof = new DAOFactoryJPA(em);
+        DAOBateau daoBateau = daof.createDAOBateau();
+        System.out.println(daoBateau.findByName("SLICE OF LIFE"));
+        System.out.println(daoBateau.findBySerialNumber("MOD1"));
+        System.out.println(daoBateau.findByModele(new Modele(1)));
         em.close();
         emf.close();
     }
